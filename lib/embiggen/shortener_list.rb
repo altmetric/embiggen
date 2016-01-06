3493ecd9ebce6155f5e9063c969523393bb9b669
Avner Cohen <israbirding@gmail.com>
2016-01-06 12:58:12 +0200

    [FIX] Setup the regexp to match domain name once, a 50%~ speed up

--- lib/embiggen/shortener_list.rb
+++ lib/embiggen/shortener_list.rb
@@ -9,17 +9,31 @@ module Embiggen
     attr_reader :domains
 
     def initialize(domains)
-      @domains = Set.new(domains)
+      @domains = Set.new(domains.map { |domain| host_pattern(domain) })
     end
 
     def include?(uri)
-      domains.any? { |domain| uri.host =~ /\b#{domain}\z/i }
+      domains.any? { |domain| uri.host =~ domain }
     end
 
     def +(other)
       self.class.new(domains + other)
     end
 
-    def_delegators :domains, :<<, :size, :delete, :empty?, :each
+    def <<(domain)
+      domains << host_pattern(domain)
+
+      self
+    end
+
+    def delete(domain)
+      domains.delete(host_pattern(domain))
+    end
+
+    def_delegators :domains, :size, :empty?, :each
+
+    def host_pattern(domain)
+      /\b#{domain}\z/i
+    end
   end
 end
